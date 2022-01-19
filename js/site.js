ad5d4ffa2c6f2e5ebcc2b2eed81a173b172ec276
Tyler0525 <96202897+Tyler0525@users.noreply.github.com>
2022-01-19 11:19:42 -0800

    Intial Commit

--- js/site.js
+++ js/site.js
@@ -1,13 +1,30 @@
-// display amesage to the user
-function getMessage() {
+        // pull the user entered from the page
+function getValues() {
+    let userString =document.getElementById("revString").value;
 
-    let userMessage = document.getElementById("message").value;
-    
-Swal.fire({
-    icon: 'success',
-    title: 'THE APP SAYS',
-    userMessage,
-})
+        //Call reverse a String
+        let revString = ReverseAString(userString);
 
-    console.log("Hello Console Log");
+        //Display the Reversed String
+        displayData(revString);
+}
+
+        //this is our generate data 
+function ReverseAString(userString){
+
+    ;
+    let revString ="";
+    for (let index = userString.length -1; index >= 0; index--) {
+        revString += userString[index];
+         }
+
+         return revString;
+}
+
+
+        //Display out Reversed String to the Page 
+function displayData(revString){
+document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+document.getElementById("alert").classList.remove('invisible');
 }
